798ef4bcd7fe2b3f19f0e1d24a47cac7ba533cd1
Arkadiy Gatin <arkadiygatin@icloud.com.com>
2023-07-09 17:41:49 +0500

    task1

--- lession1/lession1.1/Program.cs
+++ lession1/lession1.1/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, "+username);
